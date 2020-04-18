c8955e282af12876dabf1a4d796897946b2503ad
Dreamerrrr <27842508+Dreamerrrr@users.noreply.github.com>
2020-04-17 21:28:27 -0600

    Update Android.mk

--- GoogleSearch/Android.mk
+++ GoogleSearch/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := GoogleSearch
-LOCAL_SRC_FILES := .apk
+LOCAL_SRC_FILES := com.google.android.googlequicksearchbox.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MULTILIB := both
 LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
